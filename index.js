4cf172c0721fd4482a4fb98444ef2f1da2ef1bbd
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 14:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ function currentLine(currentLineofPeopleArray){
       var namesAndPositionsInLine = []
 
       for (let position = 0; position < currentLineofPeopleArray.length; position+= 1){
-          let variable = ` ${position}. ${currentLineofPeopleArray[position]}`
+          let IRLposition = postion + 1
+          let variable = ` ${IRLposition}. ${currentLineofPeopleArray[position]}`
            namesAndPositionsInLine.push(variable)
            console.log(position)
         }
