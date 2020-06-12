476694251638ce792a430800a1a81e2993b6b1c6
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 13:44:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 /*Build a function that a new customer will use when entering the deli. The function, takeANumber, should accept two parameters: the current line of people, along with the new person's name. The function should return a welcome message including the new person's position in line, such as "Welcome, Ada. You are number 1 in line.". And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 */
 
-var currentLineofPeopleArray //array of names of everyone in line
-var newPersonsName  //name of the most recent person to wanting to join currentLineofPeople-Array
+//var currentLineofPeopleArray //array of names of everyone in line
+//var newPersonsName  //name of the most recent person to wanting to join currentLineofPeople-Array
 
 
 function takeANumber(currentLineofPeopleArray, newPersonsName){
-  //add newPersonsName to end of currentLineofPeople-Array
+    //add newPersonsName to end of currentLineofPeople-Array
   currentLineofPeopleArray.push(newPersonsName)
 
-  //determine what position newPersonsName is in IRL line (i.e. not array position) by adding 1 to array position
+    //write welcomeMessage using newPersonsName & newPersonsName position in line by knowing new person is always the last person to join which is the length of the array
+  let welcomeMessage = `Welcome, ${newPersonsName}. You are number ${currentLineofPeopleArray.length} in line.`
 
-  //write welcomeMessage using newPersonsName & newPersonsName position in line
-let welcomeMessage = `Welcome, ${newPersonsName}. You are number ${currentLineofPeopleArray.length} in line.`
-
-  //return welcomeMessage using newPersonsName & newPersonsName position in line
+    //return the welcomeMessage 
   return welcomeMessage
 
 }
+
+
