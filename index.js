a9cb62310252db1f60083d25513f7e6878cc01ca
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 14:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,8 +58,9 @@ function currentLine(currentLineofPeopleArray){
       //else return message with names of people and their position in line
     else{
       //loop through array starting at [0] to get name and turn counter+1 into IRL position and store name and IRLPosition innew variable. Once done looping, return message about who is in what position.
+      var namesAndPositionsInLine
       for (let position = 0; position < currentLineofPeopleArray.lenghth; position+= 1){
-          var namesAndPositionsInLine = `The line is currently: ${position+=1}. currentLineofPeopleArray[position],`
+           namesAndPositionsInLine = `The line is currently: ${position+=1}. currentLineofPeopleArray[position],`
         }
         return namesAndPositionsInLine
     }
